7e1bb91c789b753618d028eb480d786b4fa38a90
JeongMyeongHoon <wjdaudgns1105@gmail.com>
2019-07-09 00:05:17 +0900

    7th Commit

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-//App.js 7/8 11/20pm
+//App.js 7/8 12:00am
 import React from "react";
 import {
   createBottomTabNavigator,
@@ -6,20 +6,12 @@ import {
   createAppContainer,
 } from 'react-navigation';
 import { SearchBar } from 'react-native-elements';
-import { StatusBar, StyleSheet, Button, View, Text, TouchableOpacity } from "react-native";
+import { StatusBar, StyleSheet, View, Text, TouchableOpacity } from "react-native";
 import CategoryButton from './CategoryButton';
 import LinkingButton from './LinkingButton';
 import CategoryButton2 from './longCategoryButton';
 
-class LogoTitle extends React.Component {
-  render() {
-    return (
-      <View>
-        <Text style={{fontSize: 25, color: 'white'}}>메인</Text>
-      </View>
-    );
-  }
-}
+// 메인 페이지 만드는 클래스
 class Main extends React.Component {
   static navigationOptions = {
     header: null
@@ -37,7 +29,7 @@ class Main extends React.Component {
     const { search } = this.state;
     return (
         <View style={styles.container}>
-          <StatusBar
+          <StatusBar        // 상단 Status 바 생성
             barStyle = "light-content"
             // dark-content, light-content and default
             hidden = {false}
@@ -86,25 +78,10 @@ class Main extends React.Component {
   }
 }
 
-class Search extends React.Component{
-  static navigationOptions = {
-    header: null
-  };
-  render(){
-    return(
-      <View style={styles.container}>
-          <View style={styles.content}>
-          <View style={styles.content2}>
-            </View>
-          </View>
-      </View>
-    );
-  }
-}
-
+// 우측 상단 검색 버튼 눌렀을 때 나오는 페이지 만드는 클래스
 class TagSearch extends React.Component {
   static navigationOptions = {
-    headerTitle: <LogoTitle/>
+    header: null
   };
 
   state = {
@@ -135,7 +112,7 @@ class TagSearch extends React.Component {
               color: 'white',
             }}>Back</Text>
           </TouchableOpacity>
-          <SearchBar
+          <SearchBar        // 검색창 생성
             containerStyle={{
               backgroundColor: 'black',
               borderWidth: 1, 
@@ -177,116 +154,119 @@ class TagSearch extends React.Component {
   }
 }
 
-  class SettingsScreen extends React.Component {
-    static navigationOptions = {
-      header: null,
-    };
-    render() {
-      return (
-          <View style={styles.container}>          
-            <StatusBar
-              barStyle = "light-content"
-              // dark-content, light-content and default
-              hidden = {false}
-              //To hide statusBar
-              backgroundColor = "#00BCD4"
-              //Background color of statusBar only works for Android
-              translucent = {false}
-              //allowing light, but not detailed shapes
-              networkActivityIndicatorVisible = {true}
-            />
-            <View style={styles.header}/>
-            <View style={styles.content}>
-              <View style={styles.header2}>
-                <Text style={{fontSize: 25, marginHorizontal: '5%', marginVertical: '4%', color: 'white'}}>내가 찜한 식당</Text>
-              </View>
-
-              <View style={styles.content2}>
-                <View style={{height: '18%'}}/>
-              </View>
+// 내가 찜한 식당 페이지 만드는 클래스(우측 하단 탭 눌렀을 때)
+class SettingsScreen extends React.Component {
+  static navigationOptions = {
+    header: null,
+  };
+  render() {
+    return (
+        <View style={styles.container}>          
+          <StatusBar        // 상단 Status 바 생성
+            barStyle = "light-content"
+            // dark-content, light-content and default
+            hidden = {false}
+            //To hide statusBar
+            backgroundColor = "#00BCD4"
+            //Background color of statusBar only works for Android
+            translucent = {false}
+            //allowing light, but not detailed shapes
+            networkActivityIndicatorVisible = {true}
+          />
+          <View style={styles.header}/>
+          <View style={styles.content}>
+            <View style={styles.header2}>
+              <Text style={{fontSize: 25, marginHorizontal: '5%', marginVertical: '4%', color: 'white'}}>내가 찜한 식당</Text>
             </View>
-        </View>
-      );
-    }
-  }
 
-  const HomeStack = createStackNavigator(
-    {
-      Home: Main,
-      Search: Search,
-      TagSearch: TagSearch,
-    },
-    {
-      initialRouteName: "Home",
-      headerMode: 'none',
-      defaultNavigationOptions: {
-        headerVisible: false,
-      },
-    }
-  );
-  
-  const SettingsStack = createStackNavigator(
-    {
-      Settings: SettingsScreen,
-      TagSearch: TagSearch,
-    },
-  );
-
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-    },
-    content: {
-      flex:1,
-    },
-    header: {
-      height:'5%',
-      backgroundColor:'black',
-    },  
-    footer: {
-      height:'8%',
-      backgroundColor:'black',
-    },
-    content2: {
-      flex: 1,
-    },
-    header2: {
-      backgroundColor: 'black',
-      width: '100%',
-      height: '11%',
-      flexDirection: 'row',
-      alignItems: 'center',
-      justifyContent: 'space-between',
-    },
-    tag: {
-      width: '100%',
-      height: '10%',
-      flexDirection: 'row',
-      alignItems: 'center',
-      borderBottomWidth: 2,
-      borderColor: 'gray',
-      paddingHorizontal: '2%'
-    },
-    elem: {
-      alignItems: 'center',
-      justifyContent: 'center',
-      flexDirection: 'column'
-    }
-  });
- 
-
-export default createAppContainer(createBottomTabNavigator(
-{
-  Home: HomeStack,
-  '내가 찜한 식당': SettingsStack
-},
-{
-  tabBarOptions: {
-    style: {
-      backgroundColor: 'black'
-    },
-    activeTintColor: 'white',
-    inactiveTintColor: 'gray',
+            <View style={styles.content2}>
+              <View style={{height: '18%'}}/>
+            </View>
+          </View>
+      </View>
+    );
   }
 }
+
+// 메인 페이지에 대해 스택 네비게이터 생성
+const HomeStack = createStackNavigator(
+  {
+    Home: Main,
+    TagSearch: TagSearch,
+  },
+  {
+    initialRouteName: "Home",
+    headerMode: 'none',
+    defaultNavigationOptions: {
+      headerVisible: false,
+    },
+  }
+);
+
+// 내가 찜한 식당 페이지에 대해 스택 네비게이터 생성
+const SettingsStack = createStackNavigator(
+  {
+    Settings: SettingsScreen,
+    TagSearch: TagSearch,
+  },
+);
+
+// 스타일
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  content: {
+    flex:1,
+  },
+  header: {
+    height:'5%',
+    backgroundColor:'black',
+  },  
+  footer: {
+    height:'8%',
+    backgroundColor:'black',
+  },
+  content2: {
+    flex: 1,
+  },
+  header2: {
+    backgroundColor: 'black',
+    width: '100%',
+    height: '11%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  tag: {
+    width: '100%',
+    height: '10%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderBottomWidth: 2,
+    borderColor: 'gray',
+    paddingHorizontal: '2%'
+  },
+  elem: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column'
+  }
+});
+ 
+// 탭 네비게이터 생성
+export default createAppContainer(createBottomTabNavigator(
+  {
+    Home: HomeStack,
+    '내가 찜한 식당': SettingsStack
+  },
+  {
+    tabBarOptions: {
+      style: {
+        backgroundColor: 'black'
+      },
+      activeTintColor: 'white',
+      inactiveTintColor: 'gray',
+    }
+  }
 ));
